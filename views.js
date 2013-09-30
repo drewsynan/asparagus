ffaf297d60f83d9136e710df45866209e60e5c92
Doc Design <design@docfilms.org>
2013-09-29 23:18:49 -0500

    added example splash

--- views.js
+++ views.js
@@ -2,6 +2,10 @@ function setViews(myExpressApp, myExpress) {
 	var app = myExpressApp;
 	var express = myExpress;
 
+	app.get('/', function(req, res) {
+		res.sendfile(__dirname + '/views/splash.html');
+	});
+
 	app.get('/example', function(req, res) {
 		res.sendfile(__dirname + '/views/example.html');
 	});
